a17ff7926ceaa5cefa7206645fd8df314380e518
Peter-Lazarov <peter_lazarov@hotmail.com>
2024-02-16 12:57:30 +0200

    Delete 07. Examination preparation

--- 07. Examination preparation/courseBook/utility/errorsUtility.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-exports.getErrorMessage = (error) => {
-    if(error instanceof mongoose.MongooseError) {
-        return Object.values(error.errors).at(0).message;
-    }else if (error instanceof Error) {
-        return error.message;
-    }
-};
